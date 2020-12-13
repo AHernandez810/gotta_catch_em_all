1c983623f0f397cf96934751212f482497ba4c26
ashley <ashleyhernandez810.ah@gmail.com>
2020-12-13 02:01:12 -0500

    updated get_pokemin_abilities method to extract data

--- lib/api.rb
+++ lib/api.rb
@@ -1,8 +1,8 @@
-require_relative './pokemon'
+require_relative "./pokemon"
 class API
     def initialize
         @pokemon_url = "https://pokeapi.co/api/v2/pokemon?limit=151"
-        @pokemon_abitlity = "https://pokeapi.co/api/v2/ability"
+        @pokemon_ability = "https://pokeapi.co/api/v2/ability"
     end 
 
     def get_pokemon_list
@@ -17,14 +17,32 @@ class API
     end
 
 
-    def get_pokemon_abilities
-        uri = URI.parse(@pokemon_abitlity)
-        resp = Net::HTTP.get(uri)
-        data = JSON.parse(resp)
+    def get_pokemon_abilities(pokemon)
+        #binding.pry
+        uri = URI.parse(pokemon.url)
+        response = Net::HTTP.get_response(uri)
+        data = JSON.parse(response.body)
+        data["abilities"].each do |ability_hash|
+            pokemon.abilities << ability_hash["ability"]["name"]
+        end
+        pokemon.weight = data["weight"]
+        pokemon.height = data["height"]
+        pokemon.id = data["id"]
+        data["moves"].each do |move_hash|
+            pokemon.moves << move_hash["move"]["name"]
+        end
+        data["types"].each do |type_hash|
+            pokemon.types << type_hash["type"]["name"]
+        end
+        pokemon.exp = data["base_experience"]
     end
 
     def get_poki_url
         response = HTTParty.get(@pokemon_url).parsed_response
+    end
+
+    def get_ability_url
+        response = HTTParty.get(@pokemon_ability).parsed_response
     end
 end 
 
