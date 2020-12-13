f655a7b14b231060d6b7cd50ceb573668bb07445
ashley <ashleyhernandez810.ah@gmail.com>
2020-12-13 02:10:01 -0500

    created display_info method to display data

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,12 +1,15 @@
 require_relative './environment.rb'
 class Pokemon
-    attr_accessor :name, :url
+    attr_accessor :name, :url, :types, :abilities, :weight, :height, :id, :moves, :exp
 
     @@all = []
 
     def initialize(name, url)
         self.name = name
         self.url = url
+        self.abilities = []
+        self.types = []
+        self.moves = []
         self.class.all << self 
     end 
     
@@ -22,10 +25,11 @@ class Pokemon
         puts menu_table.render(:unicode)
     end
 
-
-
-
-    
+    def display_info
+        pokemon_table = TTY::Table.new(header: ["Id", "Name", "Types", "Abilities", "Weight", "Height", "Moves", "Exp"])
+        pokemon_table << ["#{self.id}".red, "#{self.name}", "#{self.types.join(", ")}", "#{self.abilities.join(", ")}", "#{self.weight}", "#{self.height}", "#{self.moves.join(", ")[0..100]}..", "#{self.exp}"]
+        puts pokemon_table.render(:unicode)
+    end
     
 end
 #Pokemon.list_pokemon
